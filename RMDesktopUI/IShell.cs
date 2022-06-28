bfae4fedf1d2611caa60c6a75bb668f3283825b4
Ahmad A. Fouad <a.adel.dev@gmail.com>
2022-06-29 00:07:29 +0200

    Added WPF Project
    Project WPF setup with CaliburnMicro

---
+++ RMDesktopUI/IShell.cs
@@ -0,0 +1,3 @@
+namespace RMDesktopUI {
+    public interface IShell {}
+}
---
+++ RMDesktopUI/ShellViewModel.cs
@@ -0,0 +1,3 @@
+namespace RMDesktopUI {
+    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell { }
+}
